5e6b8e36ede8144b6cd7df22bd5ec17207d1c1e3
chroll0x0 <emtemp97@gmail.com>
2021-02-06 17:37:12 +0530

    add

--- config.h
+++ config.h
@@ -15,6 +15,7 @@ static const unsigned int gappx     = 0;        /* gaps */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:style=Medium:size=10" };
+/* static const char *fonts[]          = { "xos4 Terminus:size=10" }; */
 static const char dmenufont[]       = "monospace:size=10";
 /* static const char col_gray1[]       = "#222222"; */
 /* static const char col_gray2[]       = "#444444"; */
@@ -23,13 +24,13 @@ static const char dmenufont[]       = "monospace:size=10";
 /* static const char col_cyan[]        = "#937F74"; */
 static const char col_gray1[]       = "#2c2c2c";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#bebebe";
 static const char col_gray4[]       = "#bebebe";
-static const char col_cyan[]        = "#937F74";
+static const char col_cyan[]        = "#2c2c2c";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { "#A89984", col_cyan,  "#937F74"  },
 };
 
 /* tagging */

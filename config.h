3b598f81285e126754f29b025365e434bd44aaa5
chroll0x0 <emtemp97@gmail.com>
2021-02-06 15:31:39 +0530

    changes

--- config.h
+++ config.h
@@ -24,7 +24,7 @@ static const char col_gray1[]       = "#2c2c2c";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#bebebe";
-static const char col_cyan[]        = "#637A8A";
+static const char col_cyan[]        = "#937F74";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -127,7 +127,7 @@ static Button buttons[] = {
 	/* click                event mask      button          function        argument */
 	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
-	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
+	/* { ClkWinTitle,          0,              Button2,        zoom,           {0} }, */
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
 	{ ClkClientWin,         MODKEY,         Button2,        togglefloating, {0} },
